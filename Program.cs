2f5023ed378008f10c06ef0cdc5fe8436b859c0b
MartinLozano42 <lam0033068@est.univalle.edu>
2023-05-10 14:14:41 -0400

    el diego es puto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paragit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("El Diego es puto");
+            int n1, n2, suma;
+            n1=int.Parse(Console.ReadLine());
+            n2=int.Parse(Console.ReadLine());
+            suma= n1+ n2;
+            Console.WriteLine("es " + suma);
+
+
+        }
+    }
+}
